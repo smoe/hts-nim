7037ba75b10d2e85ac27a676345b69d081a280aa
Steffen Möller <moeller@debian.org>
2019-05-02 18:15:21 +0200

    Simplify install.sh
    Debian ships latest version of nim that is well capable of compiling hts-nim.

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,30 +6,9 @@ export BRANCH=${BRANCH:-devel}
 export base=$(pwd)
 
 sudo apt-get -qy install bwa make build-essential cmake libncurses-dev ncurses-dev libbz2-dev lzma-dev liblzma-dev \
-     curl  libssl-dev libtool autoconf automake libcurl4-openssl-dev
+     curl  libssl-dev libtool autoconf automake libcurl4-openssl-dev nim
 
-cd
-
-if [ ! -x  nim-$BRANCH/bin/nim ]; then
-  git clone -b $BRANCH --depth 1 git://github.com/nim-lang/nim nim-$BRANCH/
-  cd nim-$BRANCH
-  sh build_all.sh
-else
-  cd nim-$BRANCH
-  git fetch origin
-  if ! git merge FETCH_HEAD | grep "Already up-to-date"; then
-    sh build_all.sh
-  fi
-fi
-
-export PATH=$PATH:$HOME/nim-$BRANCH/bin/
 echo $PATH
-
-cd
-git clone --depth 1 https://github.com/nim-lang/nimble.git
-cd nimble
-nim c src/nimble
-src/nimble install -y
 
 cd
 set -x
